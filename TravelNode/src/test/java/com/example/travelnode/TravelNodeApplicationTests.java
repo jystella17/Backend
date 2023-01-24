671fc8c142d1b3766080a3634b1307488fb9eee8
Hannie Shin <50253860+hannie0615@users.noreply.github.com>
2023-01-24 20:38:07 +0900

    Add files via upload

---
+++ TravelNode/src/test/java/com/example/travelnode/TravelNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
