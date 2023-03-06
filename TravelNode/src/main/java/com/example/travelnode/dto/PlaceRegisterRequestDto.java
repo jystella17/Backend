d6212963e7b02d2c7d43ae14d65281db598e6294
jystella17 <jystella21617@gmail.com>
2023-03-07 01:47:39 +0900

    DTO: RoutePlace, SpotInfo 관련 DTO

--- TravelNode/src/main/java/com/example/travelnode/dto/PlaceRegisterRequestDto.java
+++ TravelNode/src/main/java/com/example/travelnode/dto/PlaceRegisterRequestDto.java
@@ -1,21 +1,27 @@
 package com.example.travelnode.dto;
 
-import com.example.travelnode.entity.SpotInfo;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.io.Serializable;
 
 @Getter
-public class SpotInfoDto implements Serializable {
+public class PlaceRegisterRequestDto implements Serializable {
     private final String spotName;
     private final String address;
+    private final String routeName;
     private final Double longitude;
     private final Double latitude;
+    private final Integer priority;
 
-    public SpotInfoDto(SpotInfo spotInfo) {
-        this.spotName = spotInfo.getSpotName();
-        this.address = spotInfo.getAddress();
-        this.longitude = spotInfo.getLongitude();
-        this.latitude = spotInfo.getLatitude();
+    @Builder
+    public PlaceRegisterRequestDto(String spotName, String address, String routeName,
+                                   Double longitude, Double latitude, Integer priority) {
+        this.spotName = spotName;
+        this.address = address;
+        this.routeName = routeName;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.priority = priority;
     }
 }
