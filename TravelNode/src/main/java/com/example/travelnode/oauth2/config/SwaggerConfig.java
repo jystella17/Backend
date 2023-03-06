254df51dc77a91d2931a6473522055810d19a14e
jystella17 <jystella21617@gmail.com>
2023-03-07 01:44:12 +0900

    Swagger 설정

--- TravelNode/src/main/java/com/example/travelnode/oauth2/config/SwaggerConfig.java
+++ TravelNode/src/main/java/com/example/travelnode/oauth2/config/SwaggerConfig.java
@@ -1,2 +1,29 @@
-package com.example.travelnode.oauth2.config;public class SwaggerConfig {
+package com.example.travelnode.oauth2.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Routing API Document",
+                description = "여행 플래너 프로젝트 API 문서",
+                version = "v1"
+        )
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Routing API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
 }
