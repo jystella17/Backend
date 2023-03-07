a054d9415eb7e48b621c30fad604d820cd6559c4
jystella17 <jystella21617@gmail.com>
2023-03-08 02:40:54 +0900

    SpotInfo CRUD API (Admin 권한 체크 필요)

--- TravelNode/src/main/java/com/example/travelnode/controller/SpotController.java
+++ TravelNode/src/main/java/com/example/travelnode/controller/SpotController.java
@@ -1,11 +1,41 @@
 package com.example.travelnode.controller;
 
+import com.example.travelnode.dto.SpotInfoDto;
+import com.example.travelnode.entity.SpotInfo;
+import com.example.travelnode.service.SpotService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/spot")
 @RequiredArgsConstructor
 public class SpotController {
+
+    private final SpotService spotService;
+
+    @GetMapping("/list")
+    public List<SpotInfo> getSpotLists() {
+
+        return spotService.getAllSpots();
+    }
+
+    @PostMapping("/register")
+    public SpotInfo registerSpot(SpotInfoDto spotInfoDto) {
+
+        return spotService.registerSpotInfo(spotInfoDto);
+    }
+
+    @PutMapping("/change-name")
+    public SpotInfo updateSpotName(String prevName, String spotName) {
+
+        return spotService.updateSpotName(prevName, spotName);
+    }
+
+    @DeleteMapping("/delete/{spotName}")
+    public void deleteSpot(@PathVariable(value = "spotName") String spotName) {
+
+        spotService.deleteSpotInfo(spotName);
+    }
 }
--- TravelNode/src/main/java/com/example/travelnode/service/SpotService.java
+++ TravelNode/src/main/java/com/example/travelnode/service/SpotService.java
@@ -1,9 +1,38 @@
 package com.example.travelnode.service;
 
+import com.example.travelnode.dto.SpotInfoDto;
+import com.example.travelnode.entity.SpotInfo;
+import com.example.travelnode.repository.SpotInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SpotService {
+
+    private final SpotInfoRepository spotInfoRepository;
+
+    public List<SpotInfo> getAllSpots() {
+
+        return spotInfoRepository.findAll();
+    }
+
+    public SpotInfo registerSpotInfo(SpotInfoDto spotInfoDto) {
+
+        return spotInfoRepository.save(spotInfoDto.toEntity());
+    }
+
+    public SpotInfo updateSpotName(String prevName, String spotName) {
+        SpotInfo spotInfo = spotInfoRepository.findSpotInfoBySpotName(prevName);
+        spotInfo.update(spotName);
+
+        return spotInfo;
+    }
+
+    public void deleteSpotInfo(String spotName) {
+        SpotInfo spotInfo = spotInfoRepository.findSpotInfoBySpotName(spotName);
+        spotInfoRepository.delete(spotInfo);
+    }
 }
