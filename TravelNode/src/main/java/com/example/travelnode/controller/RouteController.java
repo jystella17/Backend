ae1c27e48b40beb877c4329fc9408c53de198819
jystella17 <jystella21617@gmail.com>
2023-03-16 16:42:23 +0900

    Fix: URI 수정

--- TravelNode/src/main/java/com/example/travelnode/controller/RouteController.java
+++ TravelNode/src/main/java/com/example/travelnode/controller/RouteController.java
@@ -7,44 +7,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/route")
 @RequiredArgsConstructor
 public class RouteController {
 
     @Autowired
     private RouteService routeService;
 
-    @GetMapping("/route/{id}") // 루트별 루트 조회
+    @GetMapping("/{id}") // 루트별 루트 조회
     public RouteResponseDto searchById(@PathVariable Long id) {
         return routeService.searchByRouteId(id);
     }
 
-    @PostMapping("/createroute")  // 루트등록
+    @PostMapping("/create")  // 루트등록
     public Long createcity(@RequestBody RouteCreateRequestDto requestDto){
+
+
         return routeService.createcity(requestDto);
     }
 
-    @PatchMapping("/update/routecity/{id}")   // 루트도시 수정
+    @PatchMapping("/update/city/{id}")   // 루트도시 수정
     public Long updatecity(@PathVariable Long id, @RequestBody CityUpdateRequestDto requestDto) {
         return routeService.updatecity(id, requestDto);
     }
 
-    @PatchMapping("/update/routekeyword/{id}")   // 루트키워드 수정
+    @PatchMapping("/update/keyword/{id}")   // 루트키워드 수정
     public Long updatekeyword(@PathVariable Long id, @RequestBody KeywordUpdateRequestDto requestDto) {
         return routeService.updatekeyword(id, requestDto);
     }
 
-    @PatchMapping("/update/routename/{id}")   // 루트이름 수정
+    @PatchMapping("/update/route-name/{id}")   // 루트이름 수정
     public Long updatename(@PathVariable Long id, @RequestBody RouteNameUpdateRequestDto requestDto) {
         return routeService.updateroutename(id, requestDto);
     }
 
-    @PatchMapping("/update/routedate/{id}")   // 루트날짜 수정
+    @PatchMapping("/update/route-date/{id}")   // 루트날짜 수정
     public Long updateday(@PathVariable Long id, @RequestBody RouteDayUpdateRequestDto requestDto) {
         return routeService.updaterouteday(id, requestDto);
     }
 
-    @PatchMapping("/update/routeopen/{id}")   // 루트 공개여부 결정
+    @PatchMapping("/update/route-open/{id}")   // 루트 공개여부 결정
     public Long updateopen(@PathVariable Long id, @RequestBody RouteOpenUpdateRequestDto requestDto) {
         return routeService.updaterouteopen(id, requestDto);
     }
@@ -53,6 +55,4 @@ public class RouteController {
     public void deleteroute(@PathVariable Long id){
         routeService.deleteroute(id);
     }
-
-
 }
